93a29e80bbd2334cb48cffb93ebd4d909a2bb6ab
MirobidovM <miriliyosmiriobidov@gmail.com>
2022-04-30 04:11:49 +0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+alert("Teshaboy karpareshinga hush kelibsiz");
+var fullName = prompt("name");
+console.log(fullName);
+var yourAge = prompt("age");
+console.log(yourAge);
+var yourStudy = prompt("study");
+console.log(yourStudy);
